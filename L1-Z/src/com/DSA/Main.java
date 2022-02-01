96c15adb6b2015d5d6904f5d9908098062d53fa8
Priyansh Sharma <piyuindia4@gmail.com>
2022-02-01 22:39:01 +0530

    L1-Print Statment

---
+++ L1-Z/src/com/DSA/Main.java
@@ -0,0 +1,50 @@
+package com.DSA;
+
+public class Main {
+
+    public static void main(String[] args) {
+//     1.   print in java
+
+        System.out.println("Hello World");
+
+//     2. Print z
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+
+        System.out.println("*****\n   *\n  *\n *\n *****");
+
+//        print T
+
+        System.out.println("*******");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+
+//        print X
+
+        System.out.println("*      *");
+        System.out.println(" *    *");
+        System.out.println("  *  *");
+        System.out.println("    *");
+        System.out.println("  *   *");
+        System.out.println(" *      *");
+        System.out.println("*        *");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
